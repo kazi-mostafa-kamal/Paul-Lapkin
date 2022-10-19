929fd183d948bb3977cf8dbaa6e6a199ea56c0a4
mostafa kamal <115578410+kazi-mostafa-kamal@users.noreply.github.com>
2022-10-19 17:57:35 +0200

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+
     var mixer = mixitup('#sk');
     $(function() {
         $.scrollify({
@@ -8,24 +8,22 @@ $(document).ready(function() {
             setHeights: false,
         });
     });
-    $(".menu a ,.down-button a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 1800, );
-        }
-        return true
-    });
-})
-$(document).ready(function(){
-   function openMenu() {
+ 
+function openMenu() {
     document.getElementById('navbar').style.display = 'block'
 }
 
 function cloceMenu() {
     document.getElementById('navbar').style.display = 'none'
-} 
-})
+}
 
+$(".menu a ,.down-button a").on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1800, );
+    }
+    
+});
